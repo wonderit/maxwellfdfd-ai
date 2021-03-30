dc36fdf1a8a067329ac1fc4477a167bf8c8b4a17
wonderit <wonsuk@broadinstitute.org>
2021-03-30 17:37:22 +0900

    gpu setting added

--- train_ua.py
+++ train_ua.py
@@ -14,9 +14,9 @@ import matplotlib.pyplot as plt
 from pytorchtools import EarlyStopping
 import random
 
-# Device configuration
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print('Torch is running on Device : {}'.format(device))
+# # Device configuration
+# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# print('Torch is running on Device : {}'.format(device))
 #
 # # Hyper parameters
 # num_epochs = 10
@@ -202,7 +202,22 @@ if __name__ == '__main__':
     parser.add_argument("-sb", "--sigmoid_beta", help="beta of sigmoid", type=float, default=1.0)
     parser.add_argument("-uaa", "--uncertainty_attention_activation", help="flag for uncertainty attention of gradients",
                         default='sigmoid')
+
+    # arg for gpu
+    parser.add_argument("-g", "--gpu", help="set gpu num", type=int, default=0)
     args = parser.parse_args()
+
+    GPU_NUM = args.gpu
+    device = torch.device(f'cuda:{GPU_NUM}' if torch.cuda.is_available() else 'cpu')
+    torch.cuda.set_device(device)  # change allocation of current GPU
+    print('Current cuda device ', torch.cuda.current_device())  # check
+
+    # Additional Infos
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(GPU_NUM))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(GPU_NUM) / 1024 ** 3, 1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_cached(GPU_NUM) / 1024 ** 3, 1), 'GB')
 
     # TEST
     # args.unit_test = True
