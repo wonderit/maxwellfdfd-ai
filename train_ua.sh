2952163b76d46cd312c58c0485cce52d0e02a485
wonderit <wonsuk@broadinstitute.org>
2021-03-30 17:25:01 +0900

    sh edit

--- train_ua.sh
+++ train_ua.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python train_ua.py -a -rt max_diff -e 100 -it 10 -l l1 -r 0.05 > train_ua_max_diff_l1_r0.05 &
+nohup python train_ua.py -a -rt max_diff -e 100 -it 10 -l l1 -r 0.05 > train_ua_max_diff_l1_r05.log &
