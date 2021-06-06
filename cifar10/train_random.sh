9d4cc838441d7fa0657eee1f86cbae5e2041c3c7
wonderit <wonsuk@broadinstitute.org>
2021-06-07 02:50:56 +0900

    random rpo

--- cifar10/train_random.sh
+++ cifar10/train_random.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python -u train.py -a -e 100 -it 5 -k 1000 -ar -o adam > train_random.log &
+nohup python -u train.py -a -e 100 -it 5 -k 1000 -rt random -o adam > train_random.log &
