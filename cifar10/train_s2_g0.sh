e6613a2fc9ac8174c4c87186dcda2af0fb6af3e7
wonderit <won425@korea.ac.kr>
2021-06-07 18:08:29 +0900

    s2

--- cifar10/train_s2_g0.sh
+++ cifar10/train_s2_g0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python -u train.py -a -e 100 -it 10 -k 1000 > train_s2_g0.log &
+nohup python -u train.py -a -e 100 -it 10 -k 1000 -sn 2 -g 0 > train_s2_g0.log &
--- cifar10/train_s2_g1.sh
+++ cifar10/train_s2_g1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python -u train.py -a -e 100 -it 10 -k 1000 -rm -w > train_s2_g1.log &
+nohup python -u train.py -a -e 100 -it 10 -k 1000 -sn 2 -g 1 -rm -w > train_s2_g1.log &
