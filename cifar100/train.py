5a3c271eacf434b73a2f296cec21d8ef316bd1e8
wonderit <wonsuk@broadinstitute.org>
2021-06-08 08:45:31 +0900

    batch size 64

--- cifar100/train.py
+++ cifar100/train.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     parser.add_argument("-l", "--loss_function", help="Select loss functions.. (rmse,diff_rmse,diff_ce)", default="ce")
     parser.add_argument("-lr", "--learning_rate", help="Set learning_rate", type=float, default=0.001)
     parser.add_argument("-e", "--max_epoch", help="Set max epoch", type=int, default=10)
-    parser.add_argument("-b", "--batch_size", help="Set batch size", type=int, default=128)
+    parser.add_argument("-b", "--batch_size", help="Set batch size", type=int, default=64)
 
     # arg for testing parameters
     parser.add_argument("-u", "--unit_test", help="flag for testing source code", action='store_true')
