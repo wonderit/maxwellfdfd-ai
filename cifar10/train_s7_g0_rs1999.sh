27d4ff9f78dea2ef6ada8855026cad443788d852
wonderit <wonsuk@broadinstitute.org>
2021-06-08 20:12:24 +0900

    rs1999

--- cifar10/train_s7_g0_rs1999.sh
+++ cifar10/train_s7_g0_rs1999.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python -u train.py -a -e 100 -it 10 -k 1000 -ua -sb 1 -ual 0.1 -sn 7 -g 0 > train_s7_g0.log &
+nohup python -u train.py -a -e 100 -it 10 -k 1000 -ua -sb 1 -ual 0.1 -sn 7 -g 0 -rs 1999 > train_s7_g0.log &
