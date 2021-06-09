075adf3b913863b4fd159e17397471537a363d1d
wonderit <wonsuk@broadinstitute.org>
2021-06-09 22:19:57 +0900

    add

--- cifar10/train.py
+++ cifar10/train.py
@@ -58,15 +58,15 @@ def softmax(x):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--loss_function", help="Select loss functions.. (rmse,diff_rmse,diff_ce)", default="ce")
-    parser.add_argument("-lr", "--learning_rate", help="Set learning_rate", type=float, default=0.1)
-    parser.add_argument("-e", "--max_epoch", help="Set max epoch", type=int, default=200)
+    parser.add_argument("-lr", "--learning_rate", help="Set learning_rate", type=float, default=0.001)
+    parser.add_argument("-e", "--max_epoch", help="Set max epoch", type=int, default=100)
     parser.add_argument("-b", "--batch_size", help="Set batch size", type=int, default=128)
 
     # arg for testing parameters
     parser.add_argument("-u", "--unit_test", help="flag for testing source code", action='store_true')
     parser.add_argument("-d", "--debug", help="flag for debugging", action='store_true')
 
-    parser.add_argument("-o", "--optimizer", help="Select optimizer.. (sgd, adam, adamw)", default='sgd')
+    parser.add_argument("-o", "--optimizer", help="Select optimizer.. (sgd, adam, adamw)", default='adam')
     parser.add_argument("-bn", "--is_batch_norm", help="Set is_batch_norm", action='store_true')
     # arg for AL
     parser.add_argument("-it", "--iteration", help="Set iteration for AL", type=int, default=1)
@@ -348,8 +348,8 @@ if __name__ == '__main__':
             # Lr scheduler
 
             # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
-            # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 80], gamma=0.5)
-            scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[160])
+            scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 80], gamma=0.5)
+            # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[160])
             # scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_decay, last_epoch=-1)
             # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
             # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, factor=0.1,
