aa879b410bfc1f1a34825458e1226237b7ea800e
wonderit <wonsuk@broadinstitute.org>
2021-06-10 08:19:42 +0900

    lr schedule

--- cifar10/train.py
+++ cifar10/train.py
@@ -365,10 +365,10 @@ if __name__ == '__main__':
             # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
             # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 80], gamma=0.5)
             # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[160])
-            scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150])
+            # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150])
             # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[25, 35])
             # scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_decay, last_epoch=-1)
-            # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
             # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, factor=0.1,
             #                                                        min_lr=learning_rate * 0.001, verbose=True)
 
